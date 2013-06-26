2e205551d7de2153c66edc01bd39f901339ba6d4
Jan Michael Auer <ja@25th-floor.com>
2013-06-26 15:24:36 +0200

    Added a Utils service with common functions.
     - ensure stolen from angular
     - bind for parameter injection

---
+++ src/UtilsService.js
@@ -0,0 +1,51 @@
+"use strict";
+
+!function (angular) {
+
+	var Module = angular.module('25th.utils');
+	Module.factory('Utils', [function () {
+
+		/** A collection of useful utilities. */
+		var Utils = {};
+
+		/**
+		 * Ensures an object to have a key value. When the key is not present
+		 * in the target object, a factory function is executed to generate
+		 * the value.
+		 * <p/>
+		 * Source: angular.js
+		 *
+		 * @param {Object}   obj     The target object.
+		 * @param {String}   name    The key within the target object.
+		 * @param {Function} factory The factory to be executed, when the
+		 *                           key is missing.
+		 *
+		 * @returns {*} The generated object.
+		 */
+		Utils.ensure = function ensure(obj, name, factory) {
+			return obj[name] || (obj[name] = factory());
+		};
+
+		/**
+		 * Binds parameters to the given callback function. These parameters
+		 * will be injected to the callback before all parameters of the
+		 * actual function call.
+		 *
+		 * @param {Function} callback A function which will receive the bound
+		 *                            parameters.
+		 *
+		 * @returns {Function} The bound function.
+		 */
+		Utils.bind = function bind(callback) {
+			var bound = Array.prototype.slice.call(arguments, 1);
+			return function () {
+				var args = Array.prototype.slice.call(arguments);
+				callback.apply(this, bound.concat(args));
+			};
+		};
+
+		return Utils;
+
+	}]);
+
+}(angular);
