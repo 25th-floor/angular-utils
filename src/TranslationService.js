6063d07e3bee948eccb52430ddbcc653b1522fe3
Jan Michael Auer <ja@25th-floor.com>
2013-07-03 11:22:28 +0200

    Update TranslationService.js and rename stuff.

--- src/TranslationService.js
+++ src/TranslationService.js
@@ -2,33 +2,47 @@
 
 !function (angular) {
 
-	var Module = angular.module('25th.utils');
-	var _translations = {};
+	// Angular factories
 
-	Module.value('translations', function (translations) {
-		angular.extend(_translations, translations);
+	var Module = angular.module('25th.utils');
+
+	Module.run(['$locale', setLocale]);
+
+	Module.value('translations', addTranslations);
+	Module.value('translate', translate);
+	Module.value('_', translate);
+	Module.filter('i18n', function () {
+		return translate;
 	});
 
-	Module.factory('translate', ['$locale', function ($locale) {
-		return function translate(input) {
-			var whens = _translations[input];
-			var str = input;
-			var value = arguments[1];
+	// Methods
 
-			if (typeof whens === 'string') {
-				str = whens;
-			} else if (typeof whens === 'object') {
-				if (!whens[value]) value = $locale.pluralCat(value);
-				str = whens[value] || input;
-			}
+	var _locale;
+	var _translations = {};
 
-			arguments[0] = str;
-			return sprintf.apply(null, arguments);
-		};
-	}]);
+	function setLocale($locale) {
+		_locale = $locale;
+	}
 
-	Module.filter('translate', ['translate', function (translate) {
-		return translate;
-	}]);
+	function addTranslations (translations) {
+		angular.extend(_translations, translations);
+	}
+
+	function translate(input) {
+		var whens = _translations[input];
+		var str = input;
+		var value = arguments[1];
+
+		if (typeof whens === 'string') {
+			str = whens;
+		} else if (typeof whens === 'object') {
+			if (!whens[value]) value = _locale.pluralCat(value);
+			str = whens[value] || input;
+		}
+
+		arguments[0] = str;
+		return sprintf.apply(null, arguments);
+	}
 
 }(angular);
+
