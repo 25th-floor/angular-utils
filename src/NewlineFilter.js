e7d56219056f054bdb8666724b40ae52beec9c0e
rprosenc <rp@25th-floor.com>
2013-08-02 12:37:32 +0200

    Added NewlineFilter

---
+++ src/NewlineFilter.js
@@ -0,0 +1,10 @@
+"use strict";
+
+!function (angular) {
+
+	var Module = angular.module('25th.utils');
+	Module.filter('newline', function(text){
+		return text.replace(/\n/g, '<br/>');
+	});
+
+}(angular);
