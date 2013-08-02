f329eec50aae52ea3a9d035afd9924ff7ff35c6c
rprosenc <rp@25th-floor.com>
2013-08-02 14:23:29 +0200

    Fixed filter-error

--- src/NewlineFilter.js
+++ src/NewlineFilter.js
@@ -3,8 +3,10 @@
 !function (angular) {
 
 	var Module = angular.module('25th.utils');
-	Module.filter('newline', function(text){
-		return text.replace(/\n/g, '<br/>');
+	Module.filter('newline', function() {
+		function(text){
+			return text.replace(/\n/g, '<br/>');
+		}
 	});
 
 }(angular);
