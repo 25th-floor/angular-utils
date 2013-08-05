3360115da475fa908a9a5f7d0b4c7bad26b085ce
alisharif <as@25th-floor.com>
2013-08-05 12:28:50 +0200

    fix newline filter once and for all

--- src/NewlineFilter.js
+++ src/NewlineFilter.js
@@ -3,8 +3,9 @@
 !function (angular) {
 
 	var Module = angular.module('25th.utils');
-	Module.filter('newline', function(text){
-		return (text || '').replace(/\n/g, '<br/>');
+	Module.filter('newline', function() {
+		return function(text){
+			return (text || '').replace(/\n/g, '<br/>');
+		}
 	});
-
 }(angular);
